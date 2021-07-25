f1e8960031bb746c9507a66c1014974afd08f3c7
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 13:02:30 -0700

    add prod endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `fill me in when we deploy`;
+export const prodEndpoint = `https://mt-backend-t4tee.ondigitalocean.app/api/graphql`;
