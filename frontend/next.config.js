6afc50058cd5e0d73271d44fcc63df4070e4507e
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 13:13:34 -0700

    source for CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   async headers() {
     return [
       {
+        source: '*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
