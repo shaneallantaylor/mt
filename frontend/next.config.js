5cad75e85afa382ebcbd6ba28db5ffcbcbfe1391
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 14:11:15 -0700

    smaller headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,18 +6,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }],
       },
     ];
   },
