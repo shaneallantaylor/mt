df0088385b75d9f4ffd378e5e260fa40d77a1d85
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 13:16:49 -0700

    FINE docs example it is

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/*',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
