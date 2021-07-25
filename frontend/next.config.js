4baac4b7cee50883318373a99be1657459c560ab
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 13:40:47 -0700

    BYE BYE credentials

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,6 @@ module.exports = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
