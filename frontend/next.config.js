f2222936b62dff0cec6901d061da04aa1406bb34
Shane Taylor <shaneallantaylor@gmail.com>
2021-07-25 13:14:32 -0700

    SLASH life

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '*',
+        source: '/*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
